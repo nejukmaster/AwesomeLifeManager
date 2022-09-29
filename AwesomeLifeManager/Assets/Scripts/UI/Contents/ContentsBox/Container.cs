d5d55efd9e0b39ec6dc1896cb7325b4a7e6c19a7
WonWoo Lee <30349032+nejukmaster@users.noreply.github.com>
2022-09-29 13:56:41 +0000

    ver 4.3.5
    UI 개선

--- AwesomeLifeManager/Assets/Scripts/UI/Contents/ContentsBox/Container.cs
+++ AwesomeLifeManager/Assets/Scripts/UI/Contents/ContentsBox/Container.cs
@@ -16,6 +16,10 @@ public class Container : MonoBehaviour
     [SerializeField] RectTransform characterUI;
     [SerializeField] RectTransform upperCharacterTarget;
     [SerializeField] RectTransform downerCharacterTarget;
+
+    [SerializeField] RectTransform characterNameContainer;
+    [SerializeField] RectTransform upperNameTarget;
+    [SerializeField] RectTransform downerNameTarget;
     Coroutine co;
 
     public void ZoomIn(bool p_anime)
@@ -35,50 +39,58 @@ public class Container : MonoBehaviour
     public IEnumerator ZoomInCo(bool p_anime)
     {
         RectTransform t_rect = GetComponent<RectTransform>();
-        while((Vector2.Distance(t_rect.anchoredPosition, upperTarget.anchoredPosition) >= 0.3 || Vector2.Distance(t_rect.sizeDelta, upperTarget.sizeDelta) >= 0.5) && p_anime)
+        while(Vector2.Distance(t_rect.sizeDelta, upperTarget.sizeDelta) >= 0.5 && p_anime)
         {
-            t_rect.anchoredPosition = Vector2.Lerp(t_rect.anchoredPosition,
-                                                    upperTarget.anchoredPosition,
-                                                    4f * Time.deltaTime);
             t_rect.sizeDelta = Vector2.Lerp(t_rect.sizeDelta,
                                                     upperTarget.sizeDelta,
                                                     10f * Time.deltaTime);
             mask.sizeDelta = Vector2.Lerp(mask.sizeDelta,
                                                     upperMaskTarget.sizeDelta,
                                                     10f * Time.deltaTime);
+            mask.anchoredPosition = Vector2.Lerp(mask.anchoredPosition,
+                                                    upperMaskTarget.anchoredPosition,
+                                                    4f * Time.deltaTime);
             characterUI.anchoredPosition = Vector2.Lerp(characterUI.anchoredPosition,
                                                     upperCharacterTarget.anchoredPosition,
                                                     4f * Time.deltaTime);
+            characterNameContainer.anchoredPosition = Vector2.Lerp(characterNameContainer.anchoredPosition,
+                                                    upperNameTarget.anchoredPosition,
+                                                    10f * Time.deltaTime);
             yield return null;
         }
-        t_rect.anchoredPosition = upperTarget.anchoredPosition;
+        mask.anchoredPosition = upperMaskTarget.anchoredPosition;
         t_rect.sizeDelta = upperTarget.sizeDelta;
         mask.sizeDelta = upperMaskTarget.sizeDelta;
         characterUI.anchoredPosition = upperCharacterTarget.anchoredPosition;
+        characterNameContainer.anchoredPosition = upperNameTarget.anchoredPosition;
     }
 
     public IEnumerator ZoomOutCo(bool p_anime)
     {
         RectTransform t_rect = GetComponent<RectTransform>();
-        while ((Vector2.Distance(t_rect.anchoredPosition, downerTarget.anchoredPosition) >= 0.3 || Vector2.Distance(t_rect.sizeDelta, downerTarget.sizeDelta) >= 0.5) && p_anime)
+        while (Vector2.Distance(t_rect.sizeDelta, downerTarget.sizeDelta) >= 0.5 && p_anime)
         {
-            t_rect.anchoredPosition = Vector2.Lerp(t_rect.anchoredPosition,
-                                                    downerTarget.anchoredPosition,
-                                                    4f * Time.deltaTime);
             t_rect.sizeDelta = Vector2.Lerp(t_rect.sizeDelta,
                                                     downerTarget.sizeDelta,
                                                     10f * Time.deltaTime);
             mask.sizeDelta = Vector2.Lerp(mask.sizeDelta,
                                                     downerMaskTarget.sizeDelta,
                                                     10f * Time.deltaTime);
+            mask.anchoredPosition = Vector2.Lerp(mask.anchoredPosition,
+                                                    downerMaskTarget.anchoredPosition,
+                                                    4f * Time.deltaTime);
             characterUI.anchoredPosition = Vector2.Lerp(characterUI.anchoredPosition,
                                                     downerCharacterTarget.anchoredPosition,
                                                     4f * Time.deltaTime);
+            characterNameContainer.anchoredPosition = Vector2.Lerp(characterNameContainer.anchoredPosition,
+                                                    downerNameTarget.anchoredPosition,
+                                                    10f * Time.deltaTime);
             yield return null;
         }
-        t_rect.anchoredPosition = downerTarget.anchoredPosition;
+        mask.anchoredPosition = downerMaskTarget.anchoredPosition;
         t_rect.sizeDelta = downerTarget.sizeDelta;
         mask.sizeDelta = downerMaskTarget.sizeDelta;
         characterUI.anchoredPosition = downerCharacterTarget.anchoredPosition;
+        characterNameContainer.anchoredPosition = downerNameTarget.anchoredPosition;
     }
 }
