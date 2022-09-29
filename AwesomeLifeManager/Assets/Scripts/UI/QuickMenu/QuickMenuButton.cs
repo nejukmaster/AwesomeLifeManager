18be503d929cee3f4551cb55d2ae39430d401402
WonWoo Lee <30349032+nejukmaster@users.noreply.github.com>
2022-09-29 05:42:46 +0000

    ver 4.3.5
    버튼 마감

--- AwesomeLifeManager/Assets/Scripts/UI/QuickMenu/QuickMenuButton.cs
+++ AwesomeLifeManager/Assets/Scripts/UI/QuickMenu/QuickMenuButton.cs
@@ -5,10 +5,33 @@ using UnityEngine.UI;
 
 public class QuickMenuButton : MonoBehaviour
 {
-    [SerializeField] Image img;
+    UIManager theUIManager;
+    [SerializeField] RectTransform container;
+    [SerializeField] GameObject contentsBoxPopup;
+    [SerializeField] Container contentsContainer;
+    bool trig = true;
+
+    private void Start()
+    {
+        theUIManager = UIManager.instance;
+    }
 
     public void OnClick()
     {
-
+        if (trig)
+        {
+            container.sizeDelta = new Vector2(container.rect.width, container.rect.height * 3);
+            contentsBoxPopup.SetActive(true);
+            contentsContainer.ZoomIn(false);
+            trig = !trig;
+        }
+        else
+        {
+            container.sizeDelta = new Vector2(container.rect.width, container.rect.height / 3);
+            contentsBoxPopup.SetActive(false);
+            contentsContainer.ZoomOut(false);
+            trig = !trig;
+        }
+        theUIManager.externalListenerFired = true;
     }
 }
