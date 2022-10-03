f9091a1b7050f6e83f4b40851461b6d5bfea6251
WonWoo Lee <30349032+nejukmaster@users.noreply.github.com>
2022-10-03 07:30:09 +0000

    ver 4.4.3
    아 오늘 작업 많이 못했다. 아니 내가 왜 부식선탑을 가냐고 ㅅㅂㅋㅋㅋ상점이라도 안주면 주김 ㄹㅇ

--- AwesomeLifeManager/Assets/Scripts/UI/Contents/Planer/Calender/Calender.cs
+++ AwesomeLifeManager/Assets/Scripts/UI/Contents/Planer/Calender/Calender.cs
@@ -9,6 +9,7 @@ public class Calender : MonoBehaviour
     public Vector2 anchoredPos;
     public int accumFatigue = 0;
     public int accumAP = 0;
+    [SerializeField] RectTransform frame;
     RectTransform uiCanvas;
     public CalenderCellWindow cellInspectorPopup;
 
@@ -23,6 +24,7 @@ public class Calender : MonoBehaviour
             for(int j = 0; j < 7; j++)
             {
                 GameObject t_cell = ObjectPool.instance.calenderCellQueue.Dequeue();
+                t_cell.GetComponent<RectTransform>().sizeDelta = new Vector2(frame.rect.width / 7, frame.rect.height / 4);
                 t_cell.GetComponent<RectTransform>().anchoredPosition = new Vector2(j * t_cell.GetComponent<RectTransform>().rect.width,
                                                                                     -1 * i * t_cell.GetComponent<RectTransform>().rect.height);
                 t_cell.GetComponentInChildren<TextMeshProUGUI>().text = (i * 7 + j +1).ToString();
