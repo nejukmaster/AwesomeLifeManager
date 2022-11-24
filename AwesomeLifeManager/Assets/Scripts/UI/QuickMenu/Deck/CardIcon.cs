2f4c7e709a6da8b8b9d91fb2bcb35cb3650a79c5
WonWoo Lee <30349032+nejukmaster@users.noreply.github.com>
2022-11-24 06:29:14 +0000

    ver 5.0.1
    덱 시스템

---
+++ AwesomeLifeManager/Assets/Scripts/UI/QuickMenu/Deck/CardIcon.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.U2D;
+using UnityEngine.UI;
+
+public class CardIcon : MonoBehaviour
+{
+
+    [SerializeField] TextMeshProUGUI description;
+    [SerializeField] TextMeshProUGUI nameBlank;
+    [SerializeField] Image illustration;
+    [SerializeField] Image categoryImg;
+    [SerializeField] TextMeshProUGUI cost;
+
+    public void SettingCard(CardInform inform)
+    {
+        nameBlank.text = inform.name;
+        description.text = inform.description;
+        cost.text = inform.cost.ToString();
+        Sprite t_sprite = CardManager.instance.illustrationAtlas.GetSprite(inform.illusteName);
+        if (t_sprite != null)
+        {
+            illustration.sprite = t_sprite;
+        }
+        SpriteAtlas t_atlas = GetComponentInParent<UIManager>().IconAtlas;
+        switch (inform.type)
+        {
+            case CardType.Action:
+                categoryImg.sprite = t_atlas.GetSprite("Action");
+                break;
+            case CardType.Project:
+                categoryImg.sprite = t_atlas.GetSprite("Project");
+                break;
+            case CardType.Event:
+                categoryImg.sprite = t_atlas.GetSprite("Event");
+                break;
+            case CardType.Angel:
+                categoryImg.sprite = t_atlas.GetSprite("Angel");
+                break;
+        }
+    }
+}
